95b0444e34ccbc1d5faaec26e7f205e85a275b81
Claudia Ferguson <claudia.ferguson@microsoft.com>
2018-07-11 15:58:35 -0400

    Added additional puzzles for the team

---
+++ TeamProject-ThePuzzle.ps1
@@ -0,0 +1,9 @@
+#Discover the size of the TEMP folder on the local system – report total size, number of subfolders 
+#and number of files
+
+
+
+#Clean out the contents of the TEMP folder that are more than 24 hours old
+
+
+#Ensure that the deletion process doesn’t error by EXCLUDING files and directories that are pinned open. You may not be able to discover these files and will have to just find them by trial and error
