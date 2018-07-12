a6dc812b3c67de552ea96cd72c531cb5824f9f0a
Claudia Ferguson <claudia.ferguson@microsoft.com>
2018-07-12 15:26:25 -0400

    last changes

--- TeamProject-ThePuzzle.ps1
+++ TeamProject-ThePuzzle.ps1
@@ -6,4 +6,13 @@
 #Clean out the contents of the TEMP folder that are more than 24 hours old
 
 
-#Ensure that the deletion process doesn’t error by EXCLUDING files and directories that are pinned open. You may not be able to discover these files and will have to just find them by trial and error
+#Ensure that the deletion process doesn’t error by EXCLUDING files and directories that are pinned open. You may not be able to discover 
+#these files and will have to just find them by trial and error
+
+#Empty the recycle bin for the drive containing your TEMP folder
+
+
+#Create a scheduled task or job that will automate this process. Store the total size, number of files, folders and timestamp in a log file 
+#before and after the clean-up action. Also log the success or failure of the scheduled task/job. 
+#If you use scheduled jobs ensure that the old jobs are removed on a scheduled basis
+
